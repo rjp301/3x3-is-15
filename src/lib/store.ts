3ba74c16bae2f23f9f1bc782c29b2841efcac4ab
Riley Paul <rileypaul96@gmail.com>
2024-04-05 22:33:26 -0600

    add persistence to store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { Chosen, defaultChosen } from "./chosen";
 import { immer } from "zustand/middleware/immer";
+import { persist } from "zustand/middleware";
 
 interface State {
   chosen: Chosen;
@@ -12,28 +13,31 @@ interface Actions {
 }
 
 export const useChoiceStore = create<State & Actions>()(
-  immer((set, get) => ({
-    chosen: defaultChosen,
-    activeChoice: null,
-    setChoice: (key, value) =>
-      set((state) => {
-        // remove value from other keys
-        Object.entries(state.chosen).forEach(([k, v]) => {
-          if (v === value) state.chosen[k as keyof Chosen] = 0;
-        });
-
-        state.chosen[key] = value;
-      }),
-    reset: () => {
-      const currentState = get().chosen;
-      set((state) => {
-        state.chosen = defaultChosen;
-      });
-      return () => {
+  persist(
+    immer((set, get) => ({
+      chosen: defaultChosen,
+      activeChoice: null,
+      setChoice: (key, value) =>
         set((state) => {
-          state.chosen = currentState;
+          // remove value from other keys
+          Object.entries(state.chosen).forEach(([k, v]) => {
+            if (v === value) state.chosen[k as keyof Chosen] = 0;
+          });
+
+          state.chosen[key] = value;
+        }),
+      reset: () => {
+        const currentState = get().chosen;
+        set((state) => {
+          state.chosen = defaultChosen;
         });
-      };
-    },
-  }))
+        return () => {
+          set((state) => {
+            state.chosen = currentState;
+          });
+        };
+      },
+    })),
+    { name: "choices-store" }
+  )
 );
